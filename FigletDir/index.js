ace9c19f648b6556916bc84ef60647158611ff97
Muhammad Huzaifa <153427445+huzaifa157@users.noreply.github.com>
2025-08-21 15:47:27 +0500

    Update index.js

--- FigletDir/index.js
+++ FigletDir/index.js
@@ -1,6 +1,6 @@
 var figlet = require("figlet");
 
-figlet("MS PRETTY UNZILA BEGUM I LOVE YOU ", function (err, data) {
+figlet("Hello World ", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
